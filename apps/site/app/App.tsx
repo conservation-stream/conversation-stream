1d7089b11834b0f3a645a97dfcf18c8ffe28729c
James Williams <james.williams-con@mhr.co.uk>
2026-01-27 17:25:39 +0000

    Update

--- apps/site/app/App.tsx
+++ apps/site/app/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev!" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
